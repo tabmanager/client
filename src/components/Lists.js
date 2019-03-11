4d60a34648e9ed52dedcbc77b55ea18c382713c0
Jamie Goodnight <goodnightlambda@gmail.com>
2019-03-11 16:44:18 -0400

    Added a card component to Lists. It may change a lot, but I just wanted something there to map on when I get data. Also, sorry about that first commit(s) kerfuffle!

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Card, Button, CardHeader, CardFooter, CardBody,
+    CardTitle, CardText } from 'reactstrap';
 
 // ---
 
@@ -6,7 +8,18 @@ class Lists extends Component {
   render() {
     return (
         <div className="Lists">
-          You logged in? Wow! Congrats! 
+            You logged in? Wow! Congrats! 
+            <div>
+                <Card>
+                    <CardHeader>Header</CardHeader>
+                    <CardBody>
+                        <CardTitle>Special Title Treatment</CardTitle>
+                        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                        <Button>Go somewhere</Button>
+                    </CardBody>
+                    <CardFooter>Footer</CardFooter>
+                </Card>
+            </div>
         </div>
     );
   }
